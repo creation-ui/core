6692f399592be449646ae4c340b12b2f31612359
Paweł <pawelkrystkiewicz@outlook.com>
2023-09-15 21:54:24 +0200

    init: migrate from react

---
+++ src/theme/base/typography.ts
@@ -0,0 +1,7 @@
+const typography = {
+  sans: ['Plus Jakarta Sans', 'sans-serif'],
+  serif: ['Plus Jakarta Sans', 'serif'],
+  mono: ['Fira Code', 'monospace'],
+}
+
+export default typography
---
+++ src/theme/index.ts
@@ -0,0 +1,2 @@
+export * from './theme.types'
+export { default as defaultTheme } from './theme.default'
